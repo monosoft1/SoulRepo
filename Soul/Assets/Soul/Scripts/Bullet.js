02d1a0d36da88bd00a3a20bcb1499eceefe73a04
Hamza Abdullah <monosoft54@gmail.com>
2015-06-06 09:05:22 +0300

    Seventh
    7th

---
+++ Soul/Assets/Soul/Scripts/Bullet.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+var SpeedBullet : float = 500;
+var trans : Transform;
+
+function Start () 
+{
+   GetComponent.<Rigidbody>().AddForce(trans.GetComponent.<Rigidbody>().transform.forward * SpeedBullet * Time.deltaTime);
+}
---
+++ Soul/Assets/Soul/Scripts/Gun.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var Bullet : GameObject;
+var FirePoint : GameObject;
+var Ammo : int = 30;
+
+var FireMode : String = "Semi";
+
+function Start () {
+
+}
+
+function Update () 
+{
+    if(FireMode == "Semi")
+    {
+       if(Input.GetMouseButtonDown(0))
+       {
+          FireOneBullet();
+       }
+    }
+}
+
+function FireOneBullet()
+{
+    Instantiate(Bullet, FirePoint.transform.position, FirePoint.transform.rotation);
+    Ammo--;
+}
